e98d094663848f75fd2233cd56561ababdde1052
Orlson Aine Joseph <ojoseph@princeton.edu>
2022-09-22 00:40:40 -0400

    Upgrade dependencies through poetry

--- nodraft/urls.py
+++ nodraft/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, re_path
+from django.urls import include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
