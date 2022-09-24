bd2a29249eedae350bdbfb689b2839f7d1ad9d10
Orlson Aine Joseph <ojoseph@princeton.edu>
2022-09-24 03:52:21 -0400

    Add login

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from accounts.models import CustomUser
 # Create your views here.
 
 
 class Login(TemplateView):
     template_name = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('nodraft:home')
+
+        messages.error(request, 'Invalid Credentials')
+        return render(request, self.template_name, {})
 
 
 class Register(TemplateView):
--- nodraft/views.py
+++ nodraft/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 
 
 def home(request, template_name='nodraft/home.html'):
-    return render(request, template_name)
+    return render(request, template_name, {})
 
 
 def about(request, template_name='nodraft/about.html'):
-    return render(request, template_name)
+    return render(request, template_name, {})
 
 
 def features(request, template_name='nodraft/features.html'):
-    return render(request, template_name)
+    return render(request, template_name, {})
 
 
 def pricing(request, template_name='nodraft/pricing.html'):
-    return render(request, template_name)
+    return render(request, template_name, {})
