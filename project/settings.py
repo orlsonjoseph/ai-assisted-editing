05631dc573c5ebbfde33df7a36509c74018b9963
Orlson Aine Joseph <jorlsonaine@gmail.com>
2023-05-19 04:32:02 -0400

    Lint & fix environment settings

--- project/settings.py
+++ project/settings.py
@@ -12,75 +12,72 @@ BASE_DIR = Path(__file__).resolve().parent
 
 # Application definition
 INSTALLED_APPS = [
-    'nodraft.apps.NodraftConfig',
-    'accounts.apps.AccountsConfig',
-
+    "nodraft.apps.NodraftConfig",
+    "accounts.apps.AccountsConfig",
     # Third-party libraries
-    'django_browser_reload',
-    'tailwind',
-    'theme',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django_browser_reload",
+    "tailwind",
+    "theme",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = "project.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database',
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "database",
     }
 }
 
-if 'test' in sys.argv or 'test_coverage' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+if "test" in sys.argv or "test_coverage" in sys.argv:
+    DATABASES["default"]["ENGINE"] = "django.db.backends.sqlite3"
 
 # Custom authentication
-LOGIN_URL = 'accounts:login'
+LOGIN_URL = "accounts:login"
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = "accounts.CustomUser"
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'project.backends.CustomUserModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "project.backends.CustomUserModelBackend",
 ]
 
 # Password validation
@@ -88,25 +85,25 @@ AUTHENTICATION_BACKENDS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -118,29 +115,43 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
-]
+STATICFILES_DIRS = [BASE_DIR / "static/"]
 
 STATICFILES_FINDER = [
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django.contrib.staticfiles.finders.FileSystemFinder',
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
 ]
 
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# Advanced configuration
+DEBUG = bool(strtobool(os.environ.get("DEBUG", default="True")))
 
 # Tailwind CSS configuration
-TAILWIND_APP_NAME = 'theme'
+# Check if debug mode is enabled & os is windows
+if DEBUG and os.name == "nt":
+    NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+TAILWIND_APP_NAME = "theme"
 INTERNAL_IPS = ["127.0.0.1", "nodraft.local"]
 
-CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', default=False)
-SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', default=False)
+# Security configurations
+CSRF_COOKIE_SECURE = os.environ.get("CSRF_COOKIE_SECURE", default=False)
+
+SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", default=False)
+
 SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', default='#um2g($a1-#2d(enmn!3pmg6axus*wbip_y#p!ezs0*$)(^!^o')
-ENV_ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
-ALLOWED_HOSTS = ENV_ALLOWED_HOSTS.split(',') if ENV_ALLOWED_HOSTS is not None else ["nodraft.local"]
-DEBUG = bool(strtobool(os.environ.get('DEBUG', default='True')))
+    "DJANGO_SECRET_KEY", default="#um2g($a1-#2d(enmn!3pmg6axus*wbip_y#p!ezs0*$)(^!^o"
+)
+
+# Hosts configuration
+ENV_ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+
+ALLOWED_HOSTS = (
+    ENV_ALLOWED_HOSTS.split(",") if ENV_ALLOWED_HOSTS is not None else INTERNAL_IPS
+)
