5ad04ccc5c04f99db485599bd68ee3e8e58dea98
Orlson Aine Joseph <jorlsonaine@gmail.com>
2023-05-21 00:01:03 -0400

    Implement register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic.base import TemplateView
 
 from django.contrib import messages
@@ -20,11 +20,29 @@ class Login(TemplateView):
         user = authenticate(email=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect("nodraft:home")
+            messages.success(request, "Successfully logged in.")
+            return redirect("portal:home")
 
-        messages.error(request, "Invalid Credentials")
-        return render(request, self.template_name, {})
+        messages.error(request, "Invalid email or password.")
+        return HttpResponse(status=401, reason="Invalid email or password.")
 
 
 class Register(TemplateView):
     template_name = "accounts/register.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, "Email already registered.")
+            return HttpResponse(status=401, reason="Email already registered.")
+
+        user = CustomUser.objects.create_user(email=email, password=password)
+        messages.success(request, "Successfully registered.")
+
+        login(request, user)
+        return redirect("portal:home")
