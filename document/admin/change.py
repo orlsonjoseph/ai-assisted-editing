fde6a96aaec896600f098e6ec81cbf0e99e8e18e
Orlson Aine Joseph <jorlsonaine@gmail.com>
2023-05-25 07:13:49 -0400

    Define admin into separate files

---
+++ document/admin/change.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from document.models import Change
+
+
+class ChangeAdmin(admin.ModelAdmin):
+    pass
---
+++ document/admin/document.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from document.models import Document
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    pass
